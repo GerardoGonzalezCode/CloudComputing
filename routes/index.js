0dec33d0593f1b32a8bf091d5b0146981ea84bcc
Jerry <GerardoGonzalezCode@gmail.com>
2021-03-17 17:16:03 -0600

    ecrcommit

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+// index.js for easy export.
+const autoresRouter = require('./autoresRouter');
+const calculadoraRouter = require('./calculadoraRouter');
+
+module.exports = {
+    autoresRouter,
+    calculadoraRouter
+}
